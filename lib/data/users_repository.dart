78b2e236dffd5518d686341b4978318f500e4df0
Lexa <Piggsyy@gmail.com>
2024-07-08 21:16:24 +0700

    Dio fix

--- lib/data/users_repository.dart
+++ lib/data/users_repository.dart
@@ -1,18 +1,19 @@
-import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:test_task_github/data/users_response.dart';
-import 'package:http/http.dart';
 
 class UserRepository {
-  String userUrl = 'https://api.github.com/users';
+  final String _userUrl = 'https://api.github.com/users';
+  final _dio = Dio();
 
   Future<List<UserResponse>> getUsers() async {
-    final response = await get(Uri.parse(userUrl));
+    final response = await _dio.get(_userUrl);
     if (response.statusCode == 200) {
-      List<dynamic> jsonData = json.decode(response.body);
-      List<UserResponse> users = jsonData.map((json) => UserResponse.fromJson(json)).toList();
+      List<UserResponse> users = (response.data as List)
+          .map((json) => UserResponse.fromJson(json))
+          .toList();
       return users;
     } else {
-      throw Exception(response.reasonPhrase);
+      throw Exception(response.statusCode);
     }
   }
 }
